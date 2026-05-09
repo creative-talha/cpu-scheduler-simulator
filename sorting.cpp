d3bc0b356501d4f742f7e51d38fd76f7de1fb074
creative-talha <talhauetm@gmail.com>
2026-05-09 16:04:46 +0500

    Initial commit - CPU Scheduler Simulator

---
+++ sorting.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"header.h"
+
+void sorting(std::vector<process>& p) {
+    int i = 1;
+    int j = 0;
+    int size = p.size();
+
+    while (j < size) {
+        i = j + 1;
+        while (i < size) {
+            if (p[i].arrival_time < p[j].arrival_time) {
+                std::swap(p[j], p[i]);
+            }
+            i++;
+        }
+        j++;
+    }
+}
