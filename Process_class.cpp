6f76e02384b06e31e3f1e56a6af1f4f7857a1286
creative-talha <talhauetm@gmail.com>
2026-05-11 11:03:20 +0500

    Changes

--- Process_class.cpp
+++ Process_class.cpp
@@ -1,11 +1,2 @@
 
 #include"header.h"
-class process {
-public:
-    std::string id;
-    int arrival_time, brust_time, completion_time, waiting_time, turnaround_time;
-
-
-    process(std::string Id, int at, int bt) : arrival_time(at), brust_time(bt), id(Id) {}
-}
-;
--- header.h
+++ header.h
@@ -1,10 +1,16 @@
-#ifndef HEADER_H;
-#define HEADER_H;
+#ifndef HEADER_H
+#define HEADER_H
 #include<vector>;
 #include<iostream>;
-class process;
+class process {
+public:
+    std::string id;
+    int arrival_time, brust_time, completion_time, waiting_time, turnaround_time;
+
+
+    process(std::string Id, int at, int bt) : arrival_time(at), brust_time(bt), id(Id) {}
+}
+;
 void sorting(std::vector<process>& p);
 
-#endif;
-
-
+#endif
