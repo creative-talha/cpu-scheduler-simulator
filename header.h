ee2dfe8bc7a64589d6cb9df6bc1812e3232bbbe3
creative-talha <talhauetm@gmail.com>
2026-05-12 12:06:03 +0500

    changes committed

--- header.h
+++ header.h
@@ -2,6 +2,7 @@
 #define HEADER_H
 #include<vector>;
 #include<iostream>;
+//process class
 class process {
 public:
     std::string id;
@@ -11,6 +12,12 @@ public:
     process(std::string Id, int at, int bt) : arrival_time(at), brust_time(bt), id(Id) {}
 }
 ;
+
+//sorting function
 void sorting(std::vector<process>& p);
+
+//schedular class
+class schedular {};
+
 
 #endif
